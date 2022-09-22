f521e8efbb68d5a459e8a5276e55c45aee3fcfd9
umangpincha <umangpincha@gmail.com>
2022-09-22 22:13:27 +0530

    updated readme file

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let result = document.querySelector(".my-text");
 let words = document.querySelector(".my-textarea");
 let count = 0;
-let count1 = 0;
 words.addEventListener("keyup" , (event)=>{
     const key = event.key;
     if (key == "Backspace") {
